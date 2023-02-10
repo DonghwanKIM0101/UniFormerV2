adef1bb1f7087806d3568657f83cf56c0e9b2c63
DonghwanKIM0101 <kdoh2522@kaist.ac.kr>
2023-02-10 14:35:33 +0900

    torch version

--- slowfast/datasets/multigrid_helper.py
+++ slowfast/datasets/multigrid_helper.py
@@ -10,7 +10,9 @@ from torch.utils.data.sampler import Sampler
 TORCH_MAJOR = int(torch.__version__.split(".")[0])
 TORCH_MINOR = int(torch.__version__.split(".")[1])
 
-if TORCH_MAJOR >= 1 and TORCH_MINOR >= 8:
+if TORCH_MAJOR >= 2:
+    _int_classes = int
+elif TORCH_MAJOR >= 1 and TORCH_MINOR >= 8:
     _int_classes = int
 else:
     from torch._six import int_classes as _int_classes
@@ -49,10 +51,7 @@ class ShortCycleBatchSampler(Sampler):
         bs_factor = [
             int(
                 round(
-                    (
-                        float(cfg.DATA.TRAIN_CROP_SIZE)
-                        / (s * cfg.MULTIGRID.DEFAULT_S)
-                    )
+                    (float(cfg.DATA.TRAIN_CROP_SIZE) / (s * cfg.MULTIGRID.DEFAULT_S))
                     ** 2
                 )
             )
